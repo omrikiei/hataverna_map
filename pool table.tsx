e7bb2560d1abff028cf692f7573e881ca7bc1c5e
Omri Eival <oeival@grubhub.com>
2021-01-11 18:56:54 +0200

    v1

---
+++ pool table.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="pool table" tilewidth="32" tileheight="32" tilecount="24" columns="6">
+ <image source="../../Downloads/gvacca79.png" width="200" height="150"/>
+</tileset>
